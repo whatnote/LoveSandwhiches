3ede1ebc7b19080be06759d3c7299c860c6a4501
DANIEL <daniel_cornwall@hotmail.com>
2021-10-05 22:17:03 +0000

    data added to spreadsheet via def=update sale worksheet

--- run.py
+++ run.py
@@ -47,9 +47,21 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-        return False 
+        return False
 
     return True
 
 
+def update_sales_worksheet(data):
+    """
+    update sales worksheet, ass new row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
