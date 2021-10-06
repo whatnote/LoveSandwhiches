39746b4f3d618346f4849711697b1b39571688c6
DANIEL <daniel_cornwall@hotmail.com>
2021-10-06 16:07:17 +0000

    new stack data function added

--- run.py
+++ run.py
@@ -93,6 +93,21 @@ def get_last_5_entries():
     return columns
 
 
+def calculate_stock_data(data):
+    """
+    Calculating the last average stock for the next day's trading.
+    """
+    print("Calculating stock data.../n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    return new_stock_data
+
+
 def main():
     """
     Run all programs functions
@@ -102,9 +117,10 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 print('Welcome to Love Sandwhiches Data Automation')
-# main()
-
-sales_columns = get_last_5_entries()
+main()
